6b56138eb62e340febdf6df4bed3f04cb9b39302
kanchan rawat <158287791+kanchanrwt@users.noreply.github.com>
2024-02-19 20:26:21 +0530

    bootstrap starbucks
    clone site

---
+++ starbucks bootstrap/javascript.js
@@ -0,0 +1,13 @@
+//  alert ("kanchan");
+let thisBtn= document.querySelector('button')
+thisBtn.addEventListener('click',showPop);
+function showPop(){
+alert("javascript alert pop");
+}
+
+
+
+
+
+
+
